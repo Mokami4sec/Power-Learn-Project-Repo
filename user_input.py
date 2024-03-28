d0aa595d5588104b581298384665a6b377865922
Mokami4sec <128727723+Mokami4sec@users.noreply.github.com>
2024-03-28 16:43:20 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# using input() to take user details
+# Ask for user's name and store it in a variable called "name"
+name = input("Enter your name:")
+# Ask for user's age and store it in a variable called "age"
+age = input("Enter your age:")
+# Ask for user's location and store it in a variable called "location"
+location = input("Enter your location:")
+
+
+# Print out a personalized message using the user's name, age, and location
+print(f"Hello {name}, you are {age} years old and live in {location}.")
